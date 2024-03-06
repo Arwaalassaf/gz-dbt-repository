a1158712eba33e68dcbef9e44fc2b1ad3240c980
Arwa Alassaf <arwanalassaf@gmail.com>
2024-03-06 06:56:23 +0000

    margin1

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,8 +4,8 @@ select
     orders_id,
     revenue,
     quantity,
-    cast(purchse_price as INTEG) as price,
-    round(s.quantity * cast(p.purchse_price as float64), 2) as purchse_cost,
+    cast(purchse_price as float64),
+    round(s.quantity * cast(p.purchse_price as float64), 2) as purchase_cost,
     s.revenue - round(s.quantity * cast(p.purchse_price as float64), 2) as margin
 from {{ ref("stg_raw__sales") }} s
 left join {{ ref("stg_raw__product") }} p using (products_id)
